36fe5a5b431e8d962951cc616a91ebaa76e05519
Ramim2499 <mdshakibulislamramim@gmail.com>
2024-11-02 13:32:46 +0600

    templates part done

--- base/views.py
+++ base/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
 
+
+
+rooms = [
+  {'id':1, 'name':'Lets learn Python!'},
+  {'id':2, 'name':'Design with me'},
+  {'id':3, 'name':'Front end developers'},
+] 
 
 
 
 def home(request):
-  return HttpResponse("Home Page")
+  context = {'rooms':rooms}
+  return render(request, 'base/home.html', context)
+# {'how we want to specify it in the template':what we are passing in}
 
 
-def room(request):
-  return HttpResponse('Room')
+def room(request, pk):
+  room = None
+  for i in rooms:
+    if i['id'] == int(pk):
+      room = i
+
+  context = {'room':room}
+  return render(request, 'base/room.html', context)
