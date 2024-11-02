3961a921684357c8cdf5f530c8db2c2acb2271c1
Ramim2499 <mdshakibulislamramim@gmail.com>
2024-11-03 05:43:56 +0600

    New user Registration part done

--- base/views.py
+++ base/views.py
@@ -6,17 +6,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 def loginPage(request):
-
+  page = 'login'
   if request.user.is_authenticated:
     return redirect('home')
 
   if request.method == 'POST':
-    username = request.POST.get('username')
+    username = request.POST.get('username').lower()
     password = request.POST.get('password')
 
     try:
@@ -32,20 +33,39 @@ def loginPage(request):
     else:
       messages.error(request, 'Invalid username or password')
 
-  context ={}
+  context ={'page':page}
   return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+
+def registerPage(request):
+  form = UserCreationForm()
+
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'An error occur during registration.')
+
+  return render(request, 'base/login_register.html', {'form': form})
+
+
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
 
   rooms = Room.objects.filter(
-                              Q(topic__name__icontains=q) |
-                              Q(name__icontains=q) |
-                              Q(description__icontains=q)
-                              )
+    Q(topic__name__icontains=q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)
+  )
 
   topics = Topic.objects.all()
   room_count=rooms.count()
